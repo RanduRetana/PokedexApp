ed1e29c714c49b6523b6194c0100ae643c755519
Marco Randu Retana Vargas <randu2002@hotmailcom>
2023-10-10 08:18:37 -0600

    mvvm arch

---
+++ app/src/main/java/com/example/kotlin/pokedexapp/model/pokemon/GenerationV.kt
@@ -0,0 +1,6 @@
+package com.example.kotlin.pokedexapp.model.pokemon
+import com.google.gson.annotations.SerializedName
+
+data class GenerationV(
+    @SerializedName("black-white") val black_white: com.example.kotlin.pokedexapp.model.pokemon.BlackWhite
+)
